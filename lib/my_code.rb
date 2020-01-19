539688f2f2b4bb920cffb3e9a7e814bf4850f900
Gary Alberto Cordero Rosa <garycordero1690@gmail.com>
2020-01-19 19:26:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,7 +28,7 @@ def map_to_square(source_array)
   result
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
 
 end
 def reduce_to_all_true(source_array)
